35be846cd450f160356202375df27ef5ca81d8e6
EBanni <ebanni@server.fake>
2014-08-26 15:22:08 +0100

    Issues list items including updates to help pages Issues list items fixes issues list items
    formats update card form
    
    changes resize point for update card form
    
    styles checkbox on sign in page
    
    sticks footer to bottom of page
    
    corrects spacing on pricing, contact, policies and about us footer pages
    
    adds missing help image for documents and invoices
    
    adds metatags to emails
    
    adds missing closing bracket to VAT feature
    
    adds checkbox to accounts new
    
    adds lines to invoice mailer commented out for checking
    
    experiments with position: sticky for sidebar on iOS
    
    adds structure for news page articles
    
    help pages updates
    
    account cancellation help page
    
    adds new fields to the invoice mailer view
    
    turns off leter opener

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,7 +33,7 @@ Bulldog::Application.configure do
   # Change from :test to :cache for email_spec testing with Selenium
 
   config.action_mailer.delivery_method =  :cache
-  #config.action_mailer.delivery_method = :letter_opener
+  # config.action_mailer.delivery_method = :letter_opener
 
   config.action_mailer.cache_settings = { :location => "#{Rails.root}/tmp/mail.cache" }
   # config.action_mailer.perform_deliveries = :true
