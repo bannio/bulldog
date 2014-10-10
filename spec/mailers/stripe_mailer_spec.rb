2358545b387f012ab83092dfa50ca2b208c51c7d
Owen Bannister <owen@bandadbro.com>
2014-10-10 10:32:03 +0100

    add links to trial period ending email and test

--- spec/mailers/stripe_mailer_spec.rb
+++ spec/mailers/stripe_mailer_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'stripe_mock'
 
 describe StripeMailer, type: :mailer do
 
@@ -15,10 +16,10 @@ describe StripeMailer, type: :mailer do
       expect(mail).to have_body_text("free trial period ends on 06 October 2013")
     end
 
-    # it "delivers an email" do
-    #   mail.deliver
-    #   expect{mail.deliver}.to change { ActionMailer::Base.deliveries.count }.by(1)
-    # end
+    it "delivers an email" do
+      mail.deliver
+      expect{mail.deliver}.to change { ActionMailer::Base.deliveries.count }.by(1)
+    end
   end
 
   describe 'new_invoice' do
