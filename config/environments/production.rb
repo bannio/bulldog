8e7587583a1a46371b78c0613890d6182c962b0c
Owen Bannister <owen@bandadbro.com>
2016-03-17 10:48:55 +0000

    change from Mandrill to Sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,13 +71,25 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  # config.action_mailer.smtp_settings = {
+  #   :port =>           '587',
+  #   :address =>        'smtp.mandrillapp.com',
+  #   :user_name =>      ENV['MANDRILL_USERNAME'],
+  #   :password =>       ENV['MANDRILL_APIKEY'],
+  #   :domain =>         'heroku.com',
+  #   :authentication => :plain
+  # }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.default_url_options = { :host => 'bulldogclip.co.uk'}
+
   config.action_mailer.smtp_settings = {
     :port =>           '587',
-    :address =>        'smtp.mandrillapp.com',
-    :user_name =>      ENV['MANDRILL_USERNAME'],
-    :password =>       ENV['MANDRILL_APIKEY'],
+    :address =>        'smtp.sendgrid.net',
+    :user_name =>      ENV['SENDGRID_USERNAME'],
+    :password =>       ENV['SENDGRID_PASSWORD'],
     :domain =>         'heroku.com',
-    :authentication => :plain
+    :authentication => :plain,
+    :enable_starttls_auto => true
   }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'bulldogclip.co.uk'}
