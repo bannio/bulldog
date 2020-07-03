08e6206cfd0f20121d849f9aa8cdb33f7e5d1b96
Owen Bannister <owen@bandadbro.com>
2020-07-03 19:44:40 +0100

    change to single worker in config/puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -6,7 +6,7 @@
 #
 # _________________ Heroku suggested config ___________
 
-workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+workers ENV.fetch("WEB_CONCURRENCY") { 1 }
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 
