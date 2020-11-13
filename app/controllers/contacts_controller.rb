61ac2de06bd102ca0b7bca6ec4bd3131cf805b9c
Owen Bannister <owen@bandadbro.com>
2020-11-13 12:22:06 +0000

    remove contacts form and email references

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,25 +5,26 @@ class ContactsController < ApplicationController
   end
 
   def create
-    @contact = Contact.new(contact_params)
+    # # Form removed from view Nov 2020 to reduce spam email received via sendgrid
+    # @contact = Contact.new(contact_params)
 
-    if @contact.valid?
-      if @contact.mail_only?
-        flash[:notice] = "Thanks, we have added you to our mailing list."
-        @contact.add_to_mail_list
-      elsif @contact.message_only?
-        flash[:notice] = "Message sent! Thanks for getting in touch, we will get back to you as soon as we can."
-        ContactMailer.contact_email(@contact).deliver
-      else # they want both
-        flash[:notice] = "Message sent! Thanks for getting in touch, we have added you to our mailing list and we will get back to you as soon as we can."
-        ContactMailer.contact_email(@contact).deliver
-        @contact.add_to_mail_list
-      end
-      redirect_to home_path
-    else
-      flash.now[:alert] = "Your message could not be sent. Please include a message or select add to mailing list before sending."
-      render 'new'
-    end
+    # if @contact.valid?
+    #   if @contact.mail_only?
+    #     flash[:notice] = "Thanks, we have added you to our mailing list."
+    #     @contact.add_to_mail_list
+    #   elsif @contact.message_only?
+    #     flash[:notice] = "Message sent! Thanks for getting in touch, we will get back to you as soon as we can."
+    #     ContactMailer.contact_email(@contact).deliver
+    #   else # they want both
+    #     flash[:notice] = "Message sent! Thanks for getting in touch, we have added you to our mailing list and we will get back to you as soon as we can."
+    #     ContactMailer.contact_email(@contact).deliver
+    #     @contact.add_to_mail_list
+    #   end
+    #   redirect_to home_path
+    # else
+    #   flash.now[:alert] = "Your message could not be sent. Please include a message or select add to mailing list before sending."
+    #   render 'new'
+    # end
   end
 
   private
--- spec/controllers/contact_controller_spec.rb
+++ spec/controllers/contact_controller_spec.rb
@@ -17,62 +17,62 @@ describe ContactsController do
     end
   end
 
-  describe "POST#create" do
-    before do
-      allow_any_instance_of(Contact).to receive(:add_to_mail_list).and_return(true)
-    end
+  # describe "POST#create" do
+  #   before do
+  #     allow_any_instance_of(Contact).to receive(:add_to_mail_list).and_return(true)
+  #   end
 
-    context "with full valid attributes" do
-      it "assigns a new contact" do
-        post :create, params: {contact: valid_attrs}
-        expect(assigns(:contact)).to be_an_instance_of(Contact)
-      end
-      it "sends an email" do
-        expect{ post :create, params: {contact: valid_attrs} }.
-         to change { ActionMailer::Base.deliveries.count }.by(1)
-      end
-      it "redirects to home path" do
-        post :create, params: {contact: valid_attrs}
-        expect(response).to redirect_to home_path
-      end
-    end
+  #   context "with full valid attributes" do
+  #     it "assigns a new contact" do
+  #       post :create, params: {contact: valid_attrs}
+  #       expect(assigns(:contact)).to be_an_instance_of(Contact)
+  #     end
+  #     it "sends an email" do
+  #       expect{ post :create, params: {contact: valid_attrs} }.
+  #        to change { ActionMailer::Base.deliveries.count }.by(1)
+  #     end
+  #     it "redirects to home path" do
+  #       post :create, params: {contact: valid_attrs}
+  #       expect(response).to redirect_to home_path
+  #     end
+  #   end
 
-    context "with invalid attributes" do
-      it "renders new template" do
-        post :create, params: {contact: {name: ""}}
-        expect(response).to render_template 'new'
-      end
-    end
+  #   context "with invalid attributes" do
+  #     it "renders new template" do
+  #       post :create, params: {contact: {name: ""}}
+  #       expect(response).to render_template 'new'
+  #     end
+  #   end
 
-    context "with mail_only? true" do
-      it "adds to mail list" do
-        expect_any_instance_of(Contact).to receive(:add_to_mail_list)
-        post :create, params: {contact: valid_attrs.merge(message: "")}
-      end
-      it "does not send an email" do
-        expect{ post :create, params: {contact: valid_attrs.merge(message: "")} }.
-         to change { ActionMailer::Base.deliveries.count }.by(0)
-      end
-      it "redirects to home path" do
-        post :create, params: {contact: valid_attrs.merge(message: "")}
-        expect(response).to redirect_to home_path
-      end
-    end
+  #   context "with mail_only? true" do
+  #     it "adds to mail list" do
+  #       expect_any_instance_of(Contact).to receive(:add_to_mail_list)
+  #       post :create, params: {contact: valid_attrs.merge(message: "")}
+  #     end
+  #     it "does not send an email" do
+  #       expect{ post :create, params: {contact: valid_attrs.merge(message: "")} }.
+  #        to change { ActionMailer::Base.deliveries.count }.by(0)
+  #     end
+  #     it "redirects to home path" do
+  #       post :create, params: {contact: valid_attrs.merge(message: "")}
+  #       expect(response).to redirect_to home_path
+  #     end
+  #   end
 
-    context "with message_only? true" do
-      it "does not add to mail list" do
-        expect_any_instance_of(Contact).to_not receive(:add_to_mail_list)
-        post :create, params: {contact: valid_attrs.merge(mail_list: "0")}
-      end
-      it "does send an email" do
-        expect{ post :create, params: {contact: valid_attrs.merge(mail_list: "0")} }.
-         to change { ActionMailer::Base.deliveries.count }.by(1)
-      end
-      it "redirects to home path" do
-        post :create, params: {contact: valid_attrs.merge(mail_list: "0")}
-        expect(response).to redirect_to home_path
-      end
-    end
-  end
+  #   context "with message_only? true" do
+  #     it "does not add to mail list" do
+  #       expect_any_instance_of(Contact).to_not receive(:add_to_mail_list)
+  #       post :create, params: {contact: valid_attrs.merge(mail_list: "0")}
+  #     end
+  #     it "does send an email" do
+  #       expect{ post :create, params: {contact: valid_attrs.merge(mail_list: "0")} }.
+  #        to change { ActionMailer::Base.deliveries.count }.by(1)
+  #     end
+  #     it "redirects to home path" do
+  #       post :create, params: {contact: valid_attrs.merge(mail_list: "0")}
+  #       expect(response).to redirect_to home_path
+  #     end
+  #   end
+  # end
 
 end
